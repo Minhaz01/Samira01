31c0cd45926980e676e1b4f6d4202476b20d181b
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-08 07:18:56 -0600

    Update Abstruct.java
    hi

--- Project09/src/Project9/Abstruct.java
+++ Project09/src/Project9/Abstruct.java
@@ -4,6 +4,8 @@ public class Abstruct {
 
 	public static void main(String[] args) {
 	System.out.println(" Nov 8  git learner");
+	System.out.println(" Nov 8  git learner , i am from web repo");
+	System.out.println(" Nov 8  git learner, i am from web repo");	
 
 	}
 
